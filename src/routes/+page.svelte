cc61ccfe8c05c809a67b2e366007041610d3962d
Jerod <jerod@synth.sh>
2023-04-28 18:54:00 +0000

    feat(landing): basic splash page

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+</script>
+
+<div class="flex flex-col items-center justify-center w-full h-screen">
+  <div class="flex">
+    <img class="h-32" alt="The project logo" src="/pepo.png" />
+  </div>
+  <div class="flex">pepo</div>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+  daisyui: {
+    styled: true,
+    themes: ["light", "dark"],
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: "",
+    darkTheme: "dark",
+  },
+}
+
